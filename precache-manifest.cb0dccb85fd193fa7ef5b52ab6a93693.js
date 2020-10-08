2e5ea50ef538b8d1695e5722a8fbf7bdddee0502
Qasimtalkin <abulqasim110@gmail.com>
2020-10-08 15:36:33 -0400

    Updates

---
+++ precache-manifest.cb0dccb85fd193fa7ef5b52ab6a93693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d568b414f723efc520541f89d9f2f4b",
+    "url": "/React-PWA.git/index.html"
+  },
+  {
+    "revision": "04dd5fff78d7d62c7f0c",
+    "url": "/React-PWA.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cdfb32c1f06a86c9c6f2",
+    "url": "/React-PWA.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "04dd5fff78d7d62c7f0c",
+    "url": "/React-PWA.git/static/js/2.3b9901ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-PWA.git/static/js/2.3b9901ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfb32c1f06a86c9c6f2",
+    "url": "/React-PWA.git/static/js/main.30466e2a.chunk.js"
+  },
+  {
+    "revision": "15907ad67165f13a7238",
+    "url": "/React-PWA.git/static/js/runtime-main.b1ee4f16.js"
+  }
+]);
